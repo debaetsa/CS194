8e89130a60edda0d9cb12178c4712ede36353f35
Max Radermacher <max.radermacher@gmail.com>
2016-01-28 19:02:32 -0800

    Make Library a protocol (i.e., abstract class).
    The intent of this class is to define what is possible with a Library.
    This may change in the future depending on exactly what types of
    libraries we build, but this will let the API work.

--- MusicRequests/MusicRequests/Sources/Library.swift
+++ MusicRequests/MusicRequests/Sources/Library.swift
@@ -8,55 +8,16 @@
 
 import UIKit
 
-class Library: NSObject {
+protocol Library {
+
+  var songs: [Song] { get }
   
-  //Was making all the getters/setters here necessary? In the other classes I just accessed their variables because I have not yet put restritions on them. I think we should have getter and setters and keep everything private though, what do you think? I was having an issue with getting and setting syntax not playing well with the automatically generated files. I could only get it to would if I made a normal function to do it.
+  var artists: [Artist] { get }
   
-  //Also I added the setters so I could play with it in the Test class.
-  
-  var songs: [Song] {
-    get {
-      return self.songs
-    }
-    set {
-      self.songs = newValue
-    }
-  }
-  
-  var artists: [Artist] {
-    get {
-      return self.artists
-    }
-    set {
-      self.artists = newValue
-    }
-  }
-  
-  var albums: [Album] {
-    get {
-      return self.albums
-    }
-    set {
-      self.albums = newValue
-    }
-  }
-  
-  var playlists: [Playlist] {
-    get {
-      return self.playlists
-    }
-    set {
-      self.playlists = newValue
-    }
-  }
-  
-  var genres: [Genre] {
-    get {
-      return self.genres
-    }
-    set {
-      self.genres = newValue
-    }
-  }
-  
+  var albums: [Album] { get }
+
+  var playlists: [Playlist] { get }
+
+  var genres: [Genre] { get }
+
 }
