994a3b4d6342fffe328e44a3264abe4c82aad1bd
Max Radermacher <max.radermacher@gmail.com>
2016-02-05 20:44:14 -0800

    Remove comments/fix whitespace.
    Don't need to comment out big chunks of code since we have source
    control. :-)
    
    And there were a few files that hadn't yet been converted to two spaces
    for indentation.

--- MusicRequests/MusicRequests/Sources/ArtistsTableViewController.swift
+++ MusicRequests/MusicRequests/Sources/ArtistsTableViewController.swift
@@ -9,20 +9,20 @@
 import UIKit
 
 class ArtistsTableViewController: MyTableViewController {
-  
-  
+
+
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return library.allArtists.count
   }
-  
-  
+
+
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     let artists = library.allArtists
     let images = ["killers_album.png", "the_postal_service_album.png", "the_family_crest_album.png", "hozier_album.png", "sf_symphony_album.png"]
-    
+
     let cell = tableView.dequeueReusableCellWithIdentifier("SmallCell", forIndexPath: indexPath)
     let currentArtist = artists[indexPath.row]
-    
+
     cell.textLabel?.text = "\(currentArtist.name)"
 
     let albumCount = currentArtist.albums.count
@@ -35,7 +35,7 @@ class ArtistsTableViewController: MyTableViewController {
     for album in currentArtist.albums {
       songCount += album.songs.count
     }
-    
+
     var songSuffix = "songs"
     if (songCount == 1) {
       songSuffix = "song"
--- MusicRequests/MusicRequests/Sources/SongsTableViewController.swift
+++ MusicRequests/MusicRequests/Sources/SongsTableViewController.swift
@@ -9,27 +9,27 @@
 import UIKit
 
 class SongsTableViewController: MyTableViewController {
-    
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return library.allSongs.count
-    }
-    
-    
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let songs = library.allSongs
-        let images = ["killers_album.png", "the_postal_service_album.png", "the_family_crest_album.png", "hozier_album.png", "sf_symphony_album.png"]
-        
-        let cell = tableView.dequeueReusableCellWithIdentifier("SmallCell", forIndexPath: indexPath)
-      
-        let currentSong = songs[indexPath.row]
 
-        cell.textLabel?.text = "\(currentSong.name)"
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return library.allSongs.count
+  }
 
-        let artistNames = currentSong.artists.map{String($0)}.joinWithSeparator(", ")
-      
-        cell.detailTextLabel?.text = "\(artistNames) - \(songs[indexPath.row].album!.name)"
-        cell.imageView?.image = UIImage(named: images[indexPath.row % images.count])!
-      
-        return cell
-    }
+
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let songs = library.allSongs
+    let images = ["killers_album.png", "the_postal_service_album.png", "the_family_crest_album.png", "hozier_album.png", "sf_symphony_album.png"]
+
+    let cell = tableView.dequeueReusableCellWithIdentifier("SmallCell", forIndexPath: indexPath)
+
+    let currentSong = songs[indexPath.row]
+
+    cell.textLabel?.text = "\(currentSong.name)"
+
+    let artistNames = currentSong.artists.map{String($0)}.joinWithSeparator(", ")
+
+    cell.detailTextLabel?.text = "\(artistNames) - \(songs[indexPath.row].album!.name)"
+    cell.imageView?.image = UIImage(named: images[indexPath.row % images.count])!
+
+    return cell
+  }
 }
