eac517fb37816d88a43dd31e92cac958818a7842
Max Radermacher <max.radermacher@gmail.com>
2016-02-05 21:29:54 -0800

    Use the AppDelegate's "library" reference.

--- MusicRequests/MusicRequests/Sources/ItemTableViewController.swift
+++ MusicRequests/MusicRequests/Sources/ItemTableViewController.swift
@@ -10,7 +10,13 @@ import UIKit
 
 class ItemTableViewController: UITableViewController {
 
-  let library = TemporaryLibrary()
+  // This will access the shared library for the entire application.  This
+  // avoids instantiating an entire Library instance for each view controller
+  // which, in addition to being inefficient, would not actually work properly
+  // once we start using the AppleLibrary.  Because we only want a single copy
+  // of each song, we need to make sure that we don't import the data more than
+  // once.
+  let library = ((UIApplication.sharedApplication().delegate as? AppDelegate)?.library)!
 
   override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
     return 1
