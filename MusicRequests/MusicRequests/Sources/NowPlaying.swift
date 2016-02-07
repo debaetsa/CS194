e852ea8251ce1294ea2348995c45fca0372f2199
Max Radermacher <max.radermacher@gmail.com>
2016-02-07 14:15:26 -0800

    Update API for previous/current/upcoming songs.
    This wasn't very well built in the previous commit.  This fixes it up
    into something that is actually usable.
    
    The general idea is as follows: there are history, current, and upcoming
    variables, all of which contain QueueItem objects.  A QueueItem object
    adds the necessary extra information to songs in the Queue (such as the
    number of votes).
    
    When advancing to the next song, the current song moves to the history,
    and a new current song is taken from the front of upcoming.
    
    There is also a notification that the rest of the application can use to
    know when the queue changes.  This is how the UI should be updated when
    the current song changes.

--- MusicRequests/MusicRequests/Sources/NowPlaying.swift
+++ MusicRequests/MusicRequests/Sources/NowPlaying.swift
@@ -10,37 +10,47 @@ import UIKit
 
 class NowPlaying: NSObject {
 
-  private var playingQueueItem: QueueItem?
+  var playing = false
 
-  var generator: (Void -> QueueItem?)?
+  // This would ideally be "unowned", which operates very similarly to this,
+  // but we can't make that work since the unowned value shouldn't be optional.
+  // By writing it this way, it can be set after initialization, but it will
+  // still throw an error if it is ever unexpectedly NOT set.
+  weak var queue: Queue!
 
   override init() {
     super.init()
   }
 
   var isPlaying: Bool {
-    return false
+    return playing
   }
 
-  func findQueueItemToPlay() -> QueueItem? {
-    if playingQueueItem == nil {
-      if let pickNextSong = generator {
-        playingQueueItem = pickNextSong()
-      }
-    }
-    return playingQueueItem
+  func didFinishCurrentSong() {
+    // Tell the queue to advance to the next song.
+    queue.advanceToNextSong()
+
+    // And then tell this instance to play that song.
+    playCurrentSong()
   }
 
-  func didFinishCurrentQueueItem() -> Void {
-    // Reset this so that we get the
-    playingQueueItem = nil
-  }
-
-  var currentQueueItem: QueueItem? {
-    return playingQueueItem
+  func playCurrentSong() {
   }
 
   func play() {
+    assert(!isPlaying)
+
+    playing = true
+    if queue.current == nil {
+      queue.advanceToNextSong()
+    }
+    playCurrentSong()
+  }
+
+  func pause() {
+    assert(isPlaying)
+
+    playing = false
   }
 
 }
