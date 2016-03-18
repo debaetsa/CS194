fcd0d49a6217cd6d0ba515c94ef05b3077a42731
Max Radermacher <max.radermacher@gmail.com>
2016-03-17 21:28:38 -0700

    Remove the tab bar.
    This is going to be replace soon by other functionality.

--- MusicRequests/MusicRequests/Sources/QueueViewController.swift
+++ MusicRequests/MusicRequests/Sources/QueueViewController.swift
@@ -8,47 +8,13 @@
 
 import UIKit
 
-class QueueViewController: UIViewController {
-  
-  let playButtonFile = "play_button"
-  let pauseButtonFile = "pause_button"
-  
-  @IBOutlet weak var addSongButton: UIButton!
-  @IBOutlet weak var playButton: UIBarButtonItem!
-  
-  @IBAction func prevButtonPressed(sender: AnyObject) {
-    let upNextVC = self.childViewControllers[0] as! UpNextTableViewController
-    ((upNextVC.maybeQueue as! LocalQueue).nowPlaying as! LocalNowPlaying).last()
-  }
-  
-  @IBAction func playButtonPressed(sender: AnyObject) {
-    let upNextVC = self.childViewControllers[0] as! UpNextTableViewController
-    let nowPlaying = (upNextVC.maybeQueue as! LocalQueue).nowPlaying as! LocalNowPlaying
-    if (nowPlaying.isPlaying) {
-      nowPlaying.pause()
-      playButton.image = UIImage(named: playButtonFile)
-    } else {
-      nowPlaying.play()
-      playButton.image = UIImage(named: pauseButtonFile)
-    }
-  }
-  
-  @IBAction func nextButtonPressed(sender: AnyObject) {
-    let upNextVC = self.childViewControllers[0] as! UpNextTableViewController
-    let nowPlaying = (upNextVC.maybeQueue as! LocalQueue).nowPlaying as! LocalNowPlaying
-    nowPlaying.next()
-  }
-  
-  override func viewDidAppear(animated: Bool) {
-    let upNextVC = self.childViewControllers[0] as! UpNextTableViewController
-    let maybeLocalQueue = upNextVC.maybeQueue as? LocalQueue
-    if (maybeLocalQueue == nil) {
-      self.navigationController?.setToolbarHidden(true, animated: false)
-    }
-    if (maybeLocalQueue != nil) && (maybeLocalQueue!.nowPlaying as! LocalNowPlaying).isPlaying {
-      playButton.image = UIImage(named: pauseButtonFile)
-    } else {
-      playButton.image = UIImage(named: playButtonFile)
-    }
-  }
+/** This is the overall container view controller.
+
+   This contains the view controller that shows the Queue, and it also contains
+   the button to choose songs from the library. */
+class MainViewController: UIViewController {
+
+  // This class currently doesn't require an code to function properly.  But it
+  // might in the future, so I'll leave it here for now.
+
 }
